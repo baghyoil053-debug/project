43bd6a5a709c4933ac465d64f642f1b5271061eb
sun516419 <bagyoil053@gmail.com>
2026-02-07 21:10:33 +0900

    분봉코드

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 
 BASE_URL = "https://openapivts.koreainvestment.com:29443"
-APPKEY = "PSVtl4u1YdAUHz37pErtgrqgUvJ3xXwbSC18"
-APPSECRET = "oa8TAFmINbppKjw6wmPPaVn1NNubC3eeUsCDiM7zD4T3TaqdW1fU5QQ6KbXiNqoPydKciw17Rr27uSlzz9dhVQVT/Vrp6/g0oTe5+WMxz/0kr52P3/t44kHPi4xAbeOBdvRu0AqmxLaSY12GpWpfA04g7XAxhJffH00EbMsgluq07g9BHbM"
+APPKEY = "..."
+APPSECRET = "..."
 
 url = f"{BASE_URL}/oauth2/tokenP"
 headers = {
